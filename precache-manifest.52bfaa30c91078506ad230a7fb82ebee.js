90435c1bc3897af4ac6bd9500517a3db6525ab14
shivashankar1253 <varimallashankar@gmail.com>
2020-11-22 19:08:59 +0530

    Updates

---
+++ precache-manifest.52bfaa30c91078506ad230a7fb82ebee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50e1df6e0bf595da1bdef05c88524ddb",
+    "url": "/imdb/index.html"
+  },
+  {
+    "revision": "732968605b8ef107a236",
+    "url": "/imdb/static/css/main.7a94cd98.chunk.css"
+  },
+  {
+    "revision": "fc30f13adde612d22f7f",
+    "url": "/imdb/static/js/2.d790a6c1.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/imdb/static/js/2.d790a6c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "732968605b8ef107a236",
+    "url": "/imdb/static/js/main.c900cc6b.chunk.js"
+  },
+  {
+    "revision": "89e976ad9048e9c1c8bd",
+    "url": "/imdb/static/js/runtime-main.66b94abd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/imdb/static/media/logo.5d5d9eef.svg"
+  }
+]);
